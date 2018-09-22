18950d308f9ffa2644b4894686f3dfbe824372e2
Scott McPeak <scott.g.mcpeak@gmail.com>
2018-09-22 08:28:17 -0700

    control.lua: Comment out 'log' calls.

--- control.lua
+++ control.lua
@@ -14,11 +14,11 @@ function point_str(pt)
 end
 
 function deconstruct_area(box, player, force, surface)
-    log("deconstruct_area: " .. point_str(box.left_top) .. "-" .. point_str(box.right_bottom))
+    --log("deconstruct_area: " .. point_str(box.left_top) .. "-" .. point_str(box.right_bottom))
     if box_non_zero(box) then
         local cliffs = surface.find_entities_filtered({area = box, type = "cliff"})
         for k, cliff in pairs(cliffs) do
-            log("  found cliff at " .. point_str(cliff.position))
+            --log("  found cliff at " .. point_str(cliff.position))
 
             -- Check to see if there is an existing ghost.  Normally there is not
             -- because the base game deconstruction logic clears them.  However, the
@@ -40,9 +40,9 @@ function deconstruct_area(box, player, force, surface)
                     force = force
                 })
             if (#existing_ghosts > 0) then
-                log("    NOT placing ghost entity there because there already is one")
+                --log("    NOT placing ghost entity there because there already is one")
             else
-                log("    placing ghost entity there")
+                --log("    placing ghost entity there")
                 local ghost =
                     surface.create_entity(
                     {
@@ -59,7 +59,7 @@ function deconstruct_area(box, player, force, surface)
 end
 
 function cancel_deconstruct(box, player, force, surface)
-    log("cancel_deconstruct: " .. point_str(box.left_top) .. "-" .. point_str(box.right_bottom))
+    --log("cancel_deconstruct: " .. point_str(box.left_top) .. "-" .. point_str(box.right_bottom))
     if box_non_zero(box) then
         local ghosts =
             surface.find_entities_filtered(
@@ -70,7 +70,7 @@ function cancel_deconstruct(box, player, force, surface)
                 force = force
             })
         for k, ghost in pairs(ghosts) do
-            log("  removing ghost at (" .. ghost.position.x .. "," .. ghost.position.y .. ")")
+            --log("  removing ghost at (" .. ghost.position.x .. "," .. ghost.position.y .. ")")
             ghost.destroy()
         end
     end
@@ -96,7 +96,7 @@ script.on_event(
     function(event)
         local entity = event.created_entity
         if entity.name == "cliff-explosive-proxy" then
-            log("robot built cliff-explosive-proxy at " .. point_str(entity.position))
+            --log("robot built cliff-explosive-proxy at " .. point_str(entity.position))
             if
                 #entity.surface.find_entities_filtered(
                     {
@@ -108,12 +108,12 @@ script.on_event(
                     }
                 ) > 0
              then
-                log("  creating actual explosive")
+                --log("  creating actual explosive")
                 entity.surface.create_entity(
                     {name = "cliff-explosives", target = entity, position = entity.position, speed = 100}
                 )
             else
-                log("  no cliffs here, returning explosive to the robot")
+                --log("  no cliffs here, returning explosive to the robot")
                 event.robot.get_inventory(defines.inventory.robot_cargo).insert({name = "cliff-explosives", count = 1})
             end
             entity.destroy()
@@ -129,7 +129,7 @@ script.on_event(
             -- This happens when the player has a cliff-explosive in hand and clicks
             -- on nearby ground (within building distance).  A proxy is momentarily
             -- created, then immediately refunded.
-            log("player built a cliff-explosive-proxy; refunding as explosives")
+            --log("player built a cliff-explosive-proxy; refunding as explosives")
             entity.destroy()
             local player = game.players[event.player_index]
             player.insert({name = "cliff-explosives", count = 1})
@@ -140,13 +140,13 @@ script.on_event(
 script.on_configuration_changed(
     -- This is called when loading a save from a prior version of the mod.
     function()
-        log("CliffDeconstruct on_configuration_changed called")
+        --log("CliffDeconstruct on_configuration_changed called")
 
         if (global.placed_explosives) then
             -- Versions prior to 0.1.0 used a global array that is no longer
             -- needed, but which grew without bound.  Remove it so as not to
             -- waste space in memory and on disk.
-            log("clearing old placed_explosives")
+            --log("clearing old placed_explosives")
             global.placed_explosives = nil
         end
     end
